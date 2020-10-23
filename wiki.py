9d950201f3cd206b743bdcacdc4b45e8113f40ed
naum_d <olegovich1996@gmail.com>
2020-10-23 21:56:54 +0300

    Add new dataset

--- wiki.py
+++ wiki.py
@@ -1,3 +1,5 @@
+import re
+
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
@@ -8,116 +10,80 @@ from networkx.drawing.nx_agraph import graphviz_layout
 
 def test_load(start, end):
     count = 0
-    hrefs = {None: [start]}
-    file = open('./data/dataset.txt', 'a')
-
+    file = open('./data/dataset.csv', 'a')
+    hrefs = {None: {'title': None, 'urls': [start]}}
+    
     while count < 10:
         count += 1
         new_hrefs = {}
-
-        for key, urls in hrefs.items():
+        
+        for parent_url, parent in hrefs.items():
             new_hrefs = {}
-
-            for url in urls:
-                new_hrefs[url] = []
-
-                print('LOAD URL', count, url)
+            
+            for index, url in enumerate(parent['urls']):
+                print('LOAD: DEEP - {} | ITER {} of {} of {}'.format(count, index, len(parent['urls']), len(hrefs)))
                 r = requests.get(url, stream=True)
                 print('**** CODE', r.status_code)
-
+                
                 soup = BeautifulSoup(r.content, 'html.parser')
                 content = soup.findChild("div", {'id': 'content'})
                 title = content.find('h1').text
                 print('**** {}'.format(title))
-
-                file.write('{} *** {} *** {} \n'.format(title, key, url))
-
+                
+                file.write('{},{},{},{} \n'.format(parent['title'], title, parent_url, url))
+                
+                new_hrefs[url] = {'title': title, 'urls': []}
                 for tag in content.find_all():
                     if tag.text == 'Литература' or tag.text == 'Примечания':
                         break
-
+                    
                     if (
-                            tag.name != 'a'
-                            or 'href' not in tag.attrs
-                            or not tag.attrs['href'].startswith('/wiki/')
-                            or ('title' in tag.attrs and tag.attrs['title'].startswith('Википедия:'))
-                            or tag.text == 'англ.'
+                        tag.name != 'a'
+                        or 'href' not in tag.attrs
+                        or not tag.attrs['href'].startswith('/wiki/')
+                        or re.search('.[png|jpg]', tag.attrs['href'])
+                        or ('title' in tag.attrs and re.search(':', tag.attrs['title']))
+                        or re.search('[0-3][0-9]{3}', tag.text)
+                        or tag.text == 'англ.'
                     ):
                         continue
-
-                    new_hrefs[url].append('https://ru.wikipedia.org{}'.format(tag.attrs['href']))
+                    
+                    new_hrefs[url]['urls'].append('https://ru.wikipedia.org{}'.format(tag.attrs['href']))
+        
         hrefs = new_hrefs
-    print(file)
     file.close()
 
 
 def text_to_csv():
-    columns = ['title', 'parent', 'url']
+    columns = ['parent_title', 'title', 'parent_url', 'url']
     df = pd.DataFrame(data=[], columns=columns)
-    for line in open('./data/dataset.txt', 'r').readlines():
-        s = pd.Series([i.strip() for i in line.split('***')], index=columns)
-        df = df.append(s, ignore_index=True)
-
-    df.to_csv('./data/wiki.csv', index=False)
-
-
-def add_parent_title():
-    df = pd.read_csv('./data/wiki.csv')
-    res = [None]
-    prev_url = None
-
-    for index, url in enumerate(df['parent']):
-        print(index)
-        if url == 'None':
+    for line in open('./data/dataset.csv', 'r').readlines():
+        line = [i.strip() for i in line.split(',')]
+        if len(line) > 4:
             continue
-
-        if prev_url == url:
-            res.append(res[-1])
-        else:
-            r = requests.get(url, stream=True)
-            soup = BeautifulSoup(r.content, 'html.parser')
-            content = soup.findChild("div", {'id': 'content'})
-            title = content.find('h1').text
-            res.append(title)
-            prev_url = url
-            print(title)
-
-    df['parent_title'] = res
-    df.to_csv('./data/wiki.csv', index=False)
-
-
-def print_graph():
-    df = pd.read_csv('./data/wiki.csv')
-    df.loc[(df['title'] == 'Арифметика'), 'title'] = 'Арифметика !!!!!!!!!!!!!!!!!'
-    df.loc[(df['parent_title'] == 'Арифметика'), 'parent_title'] = 'Арифметика !!!!!!!!!!!!!!!!!'
-
-    G = nx.from_pandas_edgelist(df, 'parent_title', 'title')
-
-    print(nx.is_arborescence(G))
-
-    nx.draw(G, with_labels=True, node_size=0.5, font_size=0.1, width=0.05, edge_color='#AAAAAA')
-    # plt.figure(3, figsize=(100, 100))
-    plt.savefig('./data/filename.png', dpi=1200)
-    plt.show(dpi=1200)
+        s = pd.Series(line, index=columns)
+        df = df.append(s, ignore_index=True)
+    
+    df.to_csv('./data/wiki_2.csv', index=False)
 
 
 def clean_dataset():
-    df = pd.read_csv('./data/wiki.csv')
-
-    print(len(df))
-    df = df.drop_duplicates(subset=['title'])
-
-    num_rows = df.loc[(df['title'].str.match(r'[0-3][0-9]{3}')) | df['parent_title'].str.match(r'[0-3][0-9]{3}')
-                      | (df['title'].str.match('Файл:')) | (df['parent_title'].str.match('Файл:'))]
-    df = df.drop(num_rows.index)
-    print(len(df))
-
-    df.to_csv('./data/wiki.csv', index=False)
-
+    df = pd.read_csv('./data/wiki_3.csv')
+    
+    # print(len(df))
+    # df.drop_duplicates(subset=['title'], inplace=True)
+    # print(len(df))
+    #
+    # df.to_csv('./data/wiki_3.csv', index=False)
+    
     G = nx.from_pandas_edgelist(df, 'parent_title', 'title')
-
+    print(nx.is_tree(G))
+    
     pos = graphviz_layout(G, prog="twopi", args="")
-    # plt.figure(figsize=(8, 8))
-    nx.draw(G, pos, with_labels=True, node_size=0.1, font_size=0.001, width=0.01, edge_color='#AAAAAA')
-    plt.savefig('./data/wiki_tree.png', dpi=2000)
+    nx.draw(G, pos, with_labels=True, node_size=0.05, font_size=0.001, width=0.01, edge_color='#AAAAAA')
+    plt.savefig('./data/wiki_tree_1.png', dpi=3000)
+    
+    # nx.draw(G, with_labels=True, node_size=0.1, font_size=0.001, width=0.01, edge_color='#AAAAAA')
+    # plt.savefig('./data/wiki_graph_1.png', dpi=3000)
+    
     plt.show()
